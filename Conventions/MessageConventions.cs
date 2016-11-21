d054686c22fd8ee6271f22d2f0048c9a0cf0f32e
David Boike <david.boike@gmail.com>
2016-11-21 14:42:58 -0600

    Initial V5 commit

---
+++ Conventions/MessageConventions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace Conventions
+{
+    public class MessageConventions : INeedInitialization
+    {
+        public void Customize(BusConfiguration configuration)
+        {
+            configuration.Conventions()
+                .DefiningCommandsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Commands"))
+                .DefiningEventsAs(t => t.Namespace != null && t.Namespace.EndsWith(".Events"))
+                .DefiningMessagesAs(t => t.Namespace != null && t.Namespace.EndsWith(".Messages"));
+        }
+    }
+}
---
+++ Shipping.Messages/OrderShipped.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping.Messages
+{
+    public class OrderShipped
+    {
+        public string OrderId { get; set; }
+    }
+}
