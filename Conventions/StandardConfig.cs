f389eadfc01a2c10e50909f1101a74b24721ce0f
David Boike <david.boike@gmail.com>
2016-11-21 14:56:59 -0600

    Centralized EndpointConfig

---
+++ Conventions/StandardConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using NServiceBus.Persistence;
+
+namespace Conventions
+{
+    public class StandardConfig : INeedInitialization
+    {
+        public void Customize(BusConfiguration configuration)
+        {
+            configuration.UseTransport<MsmqTransport>();
+
+            configuration.UsePersistence<NHibernatePersistence>()
+                .ConnectionString(@"Server=.\sqlexpress;Database=V5toV6Demo;Trusted_Connection=true;");
+
+            configuration.UseSerialization<XmlSerializer>();
+        }
+    }
+}
